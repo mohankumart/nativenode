1140e29025fedc20a743f2451193aef0421a1690
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-05-27 19:17:53 +0530

    cli changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 
@@ -15,6 +16,13 @@ var app = {};
 app.init = function(){
     server.init();
     workers.init();
+
+    // start the CLI, but make sure it start that
+
+    setTimeout(function(){
+        debugger;
+        cli.init();
+    },5000);
 };
 
 // execute that function;
